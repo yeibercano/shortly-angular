72536307fae7316cea9fd176437e75224623128f
yeibercano <yeibercano@gmail.com>
2016-04-14 19:57:50 -0700

    Set up enviroment, working through first test

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,11 @@
-angular.module('shortly.links', [])
+angular.module('shortly.links', ['shortly.services'])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function($scope, Links) {
+  console.log('in LinksController');
+  $scope.data = {};
+  $scope.getLinks = function() {
+    // return $scope.data.links;
+    console.log('yes');
+  };
+  $scope.getLinks();
 });
