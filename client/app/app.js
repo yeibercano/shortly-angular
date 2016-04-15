13869656d2afce010108c1b2895144311b582e74
yeibercano <yeibercano@gmail.com>
2016-04-15 15:43:49 -0700

    Creates a basic shortly app

--- client/app/app.js
+++ client/app/app.js
@@ -24,6 +24,10 @@ angular.module('shortly', [
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
     })
+    .otherwise({
+      redirectTo: '/links'
+    });
+
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
