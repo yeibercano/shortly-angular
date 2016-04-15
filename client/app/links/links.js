58cf1f730a20b5bff65ba4214d04e0a4991ff95f
yeibercano <yeibercano@gmail.com>
2016-04-15 12:32:35 -0700

    passing more tests and implemeting Links controller

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,11 +1,28 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function($scope, Links) {
-  console.log('in LinksController');
   $scope.data = {};
   $scope.getLinks = function() {
-    $scope.data.links = Links;
-    return $scope.data.links;
+    // $scope.data.links = Links.getLinks();
+    // return $scope.data.links;
+    Links.getLinks()
+    .then(function(data) {
+      return response.data
+    })
   };
   $scope.getLinks();
 });
+
+//invoke links.getLinks() and passed the links stored
+// links.getLinks($scope.data.links);
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.shortly', token);
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,7 +2,16 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
-  return {}
+  var getLinks = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    });
+  }; 
+
+  return {
+    getLinks: getLinks
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
