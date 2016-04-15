791db9b642434b80e014f61eeafd474e3e57e282
yeibercano <yeibercano@gmail.com>
2016-04-15 15:14:20 -0700

    finishes basic requirements 'tests'

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,12 +2,11 @@ angular.module('shortly.shorten', ['shortly.links'])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
-  $scope.link = {};
+  $scope.link = {url: 'dummy text'};
   $scope.addLink = function() {
     Links.addLink($scope.link)
     .then(function(resp){
-      // $scope.link.url = ''; 
-      // $scope.link.url = resp.data; ASK ABOUT THIS
+      // $scope.link.url = '';
     });
   }
 });
