672c1c1b0f0836a32371f6f5751770f6516496f3
yeibercano <yeibercano@gmail.com>
2016-04-16 12:31:36 -0700

    Still attempting to order our links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,10 +2,13 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function($scope, Links) {
   $scope.data = {};
+  $scope.increment = function(item) {
+    item.visits++
+  };
   $scope.getLinks = function() {
     Links.getLinks()
     .then(function(data) {
-      console.log(data)
+      console.log(data.data[0].visits)
       $scope.data.links = data.data;
     })
     .catch(function (error) {
