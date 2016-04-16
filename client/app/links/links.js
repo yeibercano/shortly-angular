698afd35e28af313135e7ab8d494eebcf0770113
yeibercano <yeibercano@gmail.com>
2016-04-16 16:37:18 -0700

    Almost all basic requirements met. Need ordering by visits and a live-search box

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,7 +3,7 @@ angular.module('shortly.links', [])
 .controller('LinksController', function($scope, Links) {
   $scope.data = {};
   $scope.increment = function(item) {
-    item.visits++
+    item.visits = items.visits + 1;
   };
   $scope.getLinks = function() {
     Links.getLinks()
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,6 +3,10 @@ angular.module('shortly.shorten', ['shortly.links'])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = {};
+  $scope.go = function ( path ) {
+  $location.path( path );
+  };
+  
   $scope.addLink = function() {
     Links.addLink($scope.link)
     .then(function(resp) {
