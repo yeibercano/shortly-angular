40dd6151121fd35dd46e46d11773833beb17d744
yeibercano <yeibercano@gmail.com>
2016-04-15 19:56:50 -0700

    Implementing validations

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,11 +2,11 @@ angular.module('shortly.shorten', ['shortly.links'])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
-  $scope.link = {url: 'dummy'};
+  $scope.link = {};
   $scope.addLink = function() {
     Links.addLink($scope.link)
     .then(function(resp){
-      // $scope.link.url = '';
+      $scope.link.url = '';
     });
   }
 });
